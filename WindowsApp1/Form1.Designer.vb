e38cae77a56507c953bc0dd91b781937c3cc9f2a
johnrainierr <85761839+johnrainierr@users.noreply.github.com>
2021-06-12 02:26:53 +0800

    Calculator
    Calculator

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.del = New System.Windows.Forms.Button()
+        Me.plusminus = New System.Windows.Forms.Button()
+        Me.memplus = New System.Windows.Forms.Button()
+        Me.num1 = New System.Windows.Forms.Button()
+        Me.num2 = New System.Windows.Forms.Button()
+        Me.num3 = New System.Windows.Forms.Button()
+        Me.memminus = New System.Windows.Forms.Button()
+        Me.num4 = New System.Windows.Forms.Button()
+        Me.num5 = New System.Windows.Forms.Button()
+        Me.num7 = New System.Windows.Forms.Button()
+        Me.num6 = New System.Windows.Forms.Button()
+        Me.num8 = New System.Windows.Forms.Button()
+        Me.num9 = New System.Windows.Forms.Button()
+        Me.memrecall = New System.Windows.Forms.Button()
+        Me.memclear = New System.Windows.Forms.Button()
+        Me.decpoint = New System.Windows.Forms.Button()
+        Me.num0 = New System.Windows.Forms.Button()
+        Me.equal = New System.Windows.Forms.Button()
+        Me.memstore = New System.Windows.Forms.Button()
+        Me.dropdown = New System.Windows.Forms.DomainUpDown()
+        Me.insert = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(34, 35)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(305, 55)
+        Me.TextBox1.TabIndex = 0
+        '
+        'clear
+        '
+        Me.clear.BackColor = System.Drawing.Color.Tomato
+        Me.clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clear.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.clear.Location = New System.Drawing.Point(39, 114)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(112, 37)
+        Me.clear.TabIndex = 1
+        Me.clear.Text = "CLEAR"
+        Me.clear.UseVisualStyleBackColor = False
+        '
+        'del
+        '
+        Me.del.BackColor = System.Drawing.Color.Red
+        Me.del.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.del.Location = New System.Drawing.Point(157, 114)
+        Me.del.Name = "del"
+        Me.del.Size = New System.Drawing.Size(53, 37)
+        Me.del.TabIndex = 1
+        Me.del.Text = "DEL"
+        Me.del.UseVisualStyleBackColor = False
+        '
+        'plusminus
+        '
+        Me.plusminus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plusminus.Location = New System.Drawing.Point(216, 114)
+        Me.plusminus.Name = "plusminus"
+        Me.plusminus.Size = New System.Drawing.Size(53, 37)
+        Me.plusminus.TabIndex = 1
+        Me.plusminus.Text = "+/-"
+        Me.plusminus.UseVisualStyleBackColor = True
+        '
+        'memplus
+        '
+        Me.memplus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.memplus.Location = New System.Drawing.Point(275, 114)
+        Me.memplus.Name = "memplus"
+        Me.memplus.Size = New System.Drawing.Size(53, 37)
+        Me.memplus.TabIndex = 1
+        Me.memplus.Text = "M+"
+        Me.memplus.UseVisualStyleBackColor = True
+        '
+        'num1
+        '
+        Me.num1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num1.Location = New System.Drawing.Point(39, 157)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(53, 37)
+        Me.num1.TabIndex = 1
+        Me.num1.Text = "1"
+        Me.num1.UseVisualStyleBackColor = True
+        '
+        'num2
+        '
+        Me.num2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num2.Location = New System.Drawing.Point(98, 157)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(53, 37)
+        Me.num2.TabIndex = 1
+        Me.num2.Text = "2"
+        Me.num2.UseVisualStyleBackColor = True
+        '
+        'num3
+        '
+        Me.num3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num3.Location = New System.Drawing.Point(157, 157)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(53, 37)
+        Me.num3.TabIndex = 1
+        Me.num3.Text = "3"
+        Me.num3.UseVisualStyleBackColor = True
+        '
+        'memminus
+        '
+        Me.memminus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.memminus.Location = New System.Drawing.Point(275, 157)
+        Me.memminus.Name = "memminus"
+        Me.memminus.Size = New System.Drawing.Size(53, 37)
+        Me.memminus.TabIndex = 1
+        Me.memminus.Text = "M-"
+        Me.memminus.UseVisualStyleBackColor = True
+        '
+        'num4
+        '
+        Me.num4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num4.Location = New System.Drawing.Point(39, 199)
+        Me.num4.Name = "num4"
+        Me.num4.Size = New System.Drawing.Size(53, 37)
+        Me.num4.TabIndex = 1
+        Me.num4.Text = "4"
+        Me.num4.UseVisualStyleBackColor = True
+        '
+        'num5
+        '
+        Me.num5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num5.Location = New System.Drawing.Point(98, 199)
+        Me.num5.Name = "num5"
+        Me.num5.Size = New System.Drawing.Size(53, 37)
+        Me.num5.TabIndex = 1
+        Me.num5.Text = "5"
+        Me.num5.UseVisualStyleBackColor = True
+        '
+        'num7
+        '
+        Me.num7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num7.Location = New System.Drawing.Point(39, 242)
+        Me.num7.Name = "num7"
+        Me.num7.Size = New System.Drawing.Size(53, 37)
+        Me.num7.TabIndex = 1
+        Me.num7.Text = "7"
+        Me.num7.UseVisualStyleBackColor = True
+        '
+        'num6
+        '
+        Me.num6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num6.Location = New System.Drawing.Point(157, 199)
+        Me.num6.Name = "num6"
+        Me.num6.Size = New System.Drawing.Size(53, 37)
+        Me.num6.TabIndex = 1
+        Me.num6.Text = "6"
+        Me.num6.UseVisualStyleBackColor = True
+        '
+        'num8
+        '
+        Me.num8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num8.Location = New System.Drawing.Point(98, 242)
+        Me.num8.Name = "num8"
+        Me.num8.Size = New System.Drawing.Size(53, 37)
+        Me.num8.TabIndex = 1
+        Me.num8.Text = "8"
+        Me.num8.UseVisualStyleBackColor = True
+        '
+        'num9
+        '
+        Me.num9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num9.Location = New System.Drawing.Point(157, 242)
+        Me.num9.Name = "num9"
+        Me.num9.Size = New System.Drawing.Size(53, 37)
+        Me.num9.TabIndex = 1
+        Me.num9.Text = "9"
+        Me.num9.UseVisualStyleBackColor = True
+        '
+        'memrecall
+        '
+        Me.memrecall.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.memrecall.Location = New System.Drawing.Point(216, 157)
+        Me.memrecall.Name = "memrecall"
+        Me.memrecall.Size = New System.Drawing.Size(53, 37)
+        Me.memrecall.TabIndex = 1
+        Me.memrecall.Text = "MR"
+        Me.memrecall.UseVisualStyleBackColor = True
+        '
+        'memclear
+        '
+        Me.memclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.memclear.Location = New System.Drawing.Point(216, 200)
+        Me.memclear.Name = "memclear"
+        Me.memclear.Size = New System.Drawing.Size(53, 37)
+        Me.memclear.TabIndex = 1
+        Me.memclear.Text = "MC"
+        Me.memclear.UseVisualStyleBackColor = True
+        '
+        'decpoint
+        '
+        Me.decpoint.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.decpoint.Location = New System.Drawing.Point(39, 285)
+        Me.decpoint.Name = "decpoint"
+        Me.decpoint.Size = New System.Drawing.Size(53, 37)
+        Me.decpoint.TabIndex = 1
+        Me.decpoint.Text = "."
+        Me.decpoint.UseVisualStyleBackColor = True
+        '
+        'num0
+        '
+        Me.num0.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.num0.Location = New System.Drawing.Point(98, 285)
+        Me.num0.Name = "num0"
+        Me.num0.Size = New System.Drawing.Size(53, 37)
+        Me.num0.TabIndex = 1
+        Me.num0.Text = "0"
+        Me.num0.UseVisualStyleBackColor = True
+        '
+        'equal
+        '
+        Me.equal.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.equal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.equal.Location = New System.Drawing.Point(157, 285)
+        Me.equal.Name = "equal"
+        Me.equal.Size = New System.Drawing.Size(53, 37)
+        Me.equal.TabIndex = 1
+        Me.equal.Text = "="
+        Me.equal.UseVisualStyleBackColor = False
+        '
+        'memstore
+        '
+        Me.memstore.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.memstore.Location = New System.Drawing.Point(275, 200)
+        Me.memstore.Name = "memstore"
+        Me.memstore.Size = New System.Drawing.Size(53, 37)
+        Me.memstore.TabIndex = 1
+        Me.memstore.Text = "MS"
+        Me.memstore.UseVisualStyleBackColor = True
+        '
+        'dropdown
+        '
+        Me.dropdown.AllowDrop = True
+        Me.dropdown.Location = New System.Drawing.Point(216, 259)
+        Me.dropdown.Name = "dropdown"
+        Me.dropdown.Size = New System.Drawing.Size(120, 20)
+        Me.dropdown.TabIndex = 2
+        Me.dropdown.Text = "Operation"
+        '
+        'insert
+        '
+        Me.insert.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.insert.Location = New System.Drawing.Point(242, 285)
+        Me.insert.Name = "insert"
+        Me.insert.Size = New System.Drawing.Size(75, 23)
+        Me.insert.TabIndex = 3
+        Me.insert.Text = "Insert"
+        Me.insert.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 347)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(369, 354)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.insert)
+        Me.Controls.Add(Me.dropdown)
+        Me.Controls.Add(Me.memstore)
+        Me.Controls.Add(Me.memclear)
+        Me.Controls.Add(Me.memminus)
+        Me.Controls.Add(Me.memrecall)
+        Me.Controls.Add(Me.equal)
+        Me.Controls.Add(Me.memplus)
+        Me.Controls.Add(Me.num9)
+        Me.Controls.Add(Me.num3)
+        Me.Controls.Add(Me.num0)
+        Me.Controls.Add(Me.plusminus)
+        Me.Controls.Add(Me.num8)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.num6)
+        Me.Controls.Add(Me.decpoint)
+        Me.Controls.Add(Me.del)
+        Me.Controls.Add(Me.num7)
+        Me.Controls.Add(Me.num5)
+        Me.Controls.Add(Me.num1)
+        Me.Controls.Add(Me.num4)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents clear As Button
+    Friend WithEvents del As Button
+    Friend WithEvents plusminus As Button
+    Friend WithEvents memplus As Button
+    Friend WithEvents num1 As Button
+    Friend WithEvents num2 As Button
+    Friend WithEvents num3 As Button
+    Friend WithEvents memminus As Button
+    Friend WithEvents num4 As Button
+    Friend WithEvents num5 As Button
+    Friend WithEvents num7 As Button
+    Friend WithEvents num6 As Button
+    Friend WithEvents num8 As Button
+    Friend WithEvents num9 As Button
+    Friend WithEvents memrecall As Button
+    Friend WithEvents memclear As Button
+    Friend WithEvents decpoint As Button
+    Friend WithEvents num0 As Button
+    Friend WithEvents equal As Button
+    Friend WithEvents memstore As Button
+    Friend WithEvents dropdown As DomainUpDown
+    Friend WithEvents insert As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,145 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        dropdown.Items.Add("Addition")
+        dropdown.Items.Add("Subtraction")
+        dropdown.Items.Add("Multiplication")
+        dropdown.Items.Add("Division")
+    End Sub
+
+    Private Sub ButtonClickMethod(sender As Object, e As EventArgs) Handles num0.Click, num1.Click, num2.Click, num3.Click, num4.Click, num5.Click, num6.Click, num7.Click, num8.Click, num9.Click, equal.Click, decpoint.Click, clear.Click, del.Click, plusminus.Click, memclear.Click, memminus.Click, memplus.Click, memrecall.Click, memstore.Click, insert.Click
+
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "num0" Then
+            TextBox1.Text = TextBox1.Text + "0"
+        End If
+
+        If button.Name = "num1" Then
+            TextBox1.Text = TextBox1.Text + "1"
+        End If
+
+        If button.Name = "num2" Then
+            TextBox1.Text = TextBox1.Text + "2"
+        End If
+
+        If button.Name = "num3" Then
+            TextBox1.Text = TextBox1.Text + "3"
+        End If
+
+        If button.Name = "num4" Then
+            TextBox1.Text = TextBox1.Text + "4"
+        End If
+
+        If button.Name = "num5" Then
+            TextBox1.Text = TextBox1.Text + "5"
+        End If
+
+        If button.Name = "num6" Then
+            TextBox1.Text = TextBox1.Text + "6"
+        End If
+
+        If button.Name = "num7" Then
+            TextBox1.Text = TextBox1.Text + "7"
+        End If
+
+        If button.Name = "num8" Then
+            TextBox1.Text = TextBox1.Text + "8"
+        End If
+
+        If button.Name = "num9" Then
+            TextBox1.Text = TextBox1.Text + "9"
+        End If
+
+        If button.Name = "decpoint" Then
+            TextBox1.Text = TextBox1.Text + ""
+        End If
+
+        If button.Name = "add" Then
+            TextBox1.Text = TextBox1.Text + "+"
+        End If
+
+        If button.Name = "subtract" Then
+            TextBox1.Text = TextBox1.Text + "-"
+        End If
+
+        If button.Name = "divide" Then
+            TextBox1.Text = TextBox1.Text + "/"
+        End If
+
+        If button.Name = "multiply" Then
+            TextBox1.Text = TextBox1.Text + "*"
+        End If
+
+        If button.Name = "clear" Then
+            TextBox1.Text = ""
+        End If
+
+        If button.Name = "decpoint" Then
+            TextBox1.Text = TextBox1.Text + "."
+        End If
+
+        If button.Name = "equal" Then
+            Dim equation As String = TextBox1.Text
+            Dim result = New DataTable().Compute(equation, Nothing)
+            TextBox1.Text = result
+
+        End If
+
+        If button.Name = "insert" Then
+            If dropdown.Text = "Addition" Then
+                TextBox1.Text = TextBox1.Text + "+"
+            ElseIf dropdown.Text = "Subtraction" Then
+                TextBox1.Text = TextBox1.Text + "-"
+            ElseIf dropdown.Text = "Multiplication" Then
+                TextBox1.Text = TextBox1.Text + "*"
+            ElseIf dropdown.Text = "Division" Then
+                TextBox1.Text = TextBox1.Text + "/"
+            End If
+        End If
+
+        If button.Name = "plusminus" Then
+            TextBox1.Text = "-" + TextBox1.Text
+        End If
+
+        If button.Name = "memstore" Then
+            Label1.Text = TextBox1.Text
+        End If
+
+        If button.Name = "memrecall" Then
+            TextBox1.Text = Label1.Text
+        End If
+
+        If button.Name = "memplus" Then
+            TextBox1.Text = TextBox1.Text + "+" + Label1.Text
+        End If
+
+        If button.Name = "memminus" Then
+            TextBox1.Text = TextBox1.Text + "-" + Label1.Text
+
+        End If
+
+
+        If button.Name = "del" Then
+            If TextBox1.Text.Length > 0 Then
+                TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1, 1)
+            End If
+        End If
+
+    End Sub
+
+    Private Sub dropdown_SelectedItemChanged(sender As Object, e As EventArgs) Handles dropdown.SelectedItemChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub insert_Click(sender As Object, e As EventArgs) Handles insert.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
